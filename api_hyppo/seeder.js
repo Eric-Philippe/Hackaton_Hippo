3c7a56c7e2eb9e1b7cccc497baddcd6461a6d3eb
entrezunfredici <superfred2468@gmail.com>
2025-01-23 10:14:33 +0100

    [ADD] connect seeder

--- api_hyppo/seeder.js
+++ api_hyppo/seeder.js
@@ -28,6 +28,16 @@ const seedDatabase = async () => {
 
     await db.instance.query("PRAGMA foreign_keys = OFF");
 
+    const users = require("./seeds/User.seed.json");
+    users.forEach((user) => {
+      user.password = bcrypt.hashSync(user.password, 10);
+    });
+    await db.User.bulkCreate(users);
+    console.log("Users table seeded successfully");
+
+    const roles = require("./seeds/Role.seed.json");
+    await db.Role.bulkCreate(roles);
+    console.log("Roles table seeded successfully");
 
     console.log("Sample data inserted");
   } catch (error) {
