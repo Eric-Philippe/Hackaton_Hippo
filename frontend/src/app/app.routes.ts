9e80791d3aec0a28382f045e11954e3726aa0119
Eric-Philippe <ericphlpp@gmail.com>
2025-01-23 12:11:19 +0100

    Map

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,13 +1,16 @@
 import { HomeComponent } from './Pages/home/home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MapComponent } from './Pages/Map/map.component';
 
-export const routes: Routes = [ { path: '', component: HomeComponent }, // Route par défaut
-    { path: '**', redirectTo: '', pathMatch: 'full' } // Redirection pour les routes non trouvées
-  ];
-  
-  @NgModule({
-    imports: [RouterModule.forRoot(routes)],
-    exports: [RouterModule]
-  })
-  export class AppRoutingModule { }
+export const routes: Routes = [
+  { path: 'home', component: HomeComponent }, // Route par défaut
+  { path: '', component: MapComponent }, // Route pour la page de la carte
+  { path: '**', redirectTo: '', pathMatch: 'full' }, // Redirection pour les routes non trouvées
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
