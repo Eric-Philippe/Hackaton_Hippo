c882d92445f5dd20ab7c9361dc474ae62ace4737
Bryce Fuertes <brycefuertes07@gmail.com>
2025-01-23 10:41:12 +0100

    Add header + home

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,13 @@
-import { Routes } from '@angular/router';
+import { HomeComponent } from './Pages/home/home.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [ { path: '', component: HomeComponent }, // Route par défaut
+    { path: '**', redirectTo: '', pathMatch: 'full' } // Redirection pour les routes non trouvées
+  ];
+  
+  @NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+  })
+  export class AppRoutingModule { }
