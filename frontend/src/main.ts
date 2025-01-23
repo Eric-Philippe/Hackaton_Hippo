c74b3f0574ae2cc4a9e6ec333fb530dab72eec02
Bryce Fuertes <brycefuertes07@gmail.com>
2025-01-23 13:59:48 +0100

    fix erreur provider

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -3,10 +3,19 @@ import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import { provideHttpClient } from '@angular/common/http';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeng/themes/aura';
 
 bootstrapApplication(AppComponent, {
   ...appConfig,
-  providers: [provideHttpClient(),
-    provideAnimations()]
+  providers: [
+    provideHttpClient(),
+    provideAnimations(),  
+    providePrimeNG({
+      theme: {
+        preset: Aura,
+      },
+    }),
+  ]
 })
 .catch((err) => console.error(err));
