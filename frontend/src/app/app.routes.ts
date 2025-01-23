5d2e788805a71331811ffda0ea09ebecdcf2464e
TimotheGury <168420661+TimotheGury@users.noreply.github.com>
2025-01-23 11:30:09 +0100

    info page

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,13 +1,16 @@
 import { HomeComponent } from './Pages/home/home.component';
+import { InfoComponent } from './Pages/info/info.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-export const routes: Routes = [ { path: '', component: HomeComponent }, // Route par défaut
-    { path: '**', redirectTo: '', pathMatch: 'full' } // Redirection pour les routes non trouvées
-  ];
-  
-  @NgModule({
-    imports: [RouterModule.forRoot(routes)],
-    exports: [RouterModule]
-  })
-  export class AppRoutingModule { }
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'info', component: InfoComponent }, // Route par défaut
+  { path: '**', redirectTo: '', pathMatch: 'full' },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
