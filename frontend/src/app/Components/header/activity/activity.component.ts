067c77164847da45553839a54093a37bce61985a
Baptiste BESSON <baptiste.besson63@gmail.com>
2025-01-23 14:21:41 +0100

    modif orbo

--- frontend/src/app/Components/header/activity/activity.component.ts
+++ frontend/src/app/Components/header/activity/activity.component.ts
@@ -1,15 +1,15 @@
 import { Component } from '@angular/core';
-import { Activity, ActivityService } from '../../Services/activity.service';
+import { Activity, ActivityService } from '../../../Services/activity.service';
 import { HttpClientModule } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-activity-test',
+  selector: 'app-activity',
   imports: [HttpClientModule, CommonModule],
-  templateUrl: './activity-test.component.html',
-  styleUrl: './activity-test.component.scss'
+  templateUrl: './activity.component.html',
+  styleUrl: './activity.component.scss'
 })
-export class ActivityTestComponent {
+export class ActivityComponent {
   activities: Activity[] = [];
 
   constructor(private activityService: ActivityService) {}
--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from "./Pages/home/home.component";
-import { ActivityTestComponent } from "./Pages/activity-test/activity-test.component";
+import { ActivityComponent } from "./Components/header/activity/activity.component";
+
 
 @Component({
   selector: 'app-root',
-  imports: [HttpClientModule, ActivityTestComponent, HomeComponent],
+  imports: [HttpClientModule, HomeComponent, ActivityComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
