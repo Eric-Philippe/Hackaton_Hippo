de3bc29e11f72a0208b0235f4b4df99f5149e2da
Baptiste BESSON <baptiste.besson63@gmail.com>
2025-01-24 10:38:42 +0100

    debut config menu

--- frontend/src/app/Components/header/header.component.ts
+++ frontend/src/app/Components/header/header.component.ts
@@ -4,14 +4,27 @@ import { MatDrawerContainer, MatDrawer, MatSidenavModule } from '@angular/materi
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { Router } from '@angular/router';
+import { MatMenuModule } from '@angular/material/menu';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [MatSidenavModule, MatToolbarModule, MatIconModule, MatButtonModule],
+  imports: [CommonModule, MatSidenavModule, MatToolbarModule, MatIconModule, MatButtonModule,MatMenuModule],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
   providers: [],
 })
 export class HeaderComponent   {
+  showMenu = false; // Par défaut, le menu n'est pas affiché
+
+  constructor(private router: Router) {}
+
+
+  ngOnInit(): void {
+    this.router.events.subscribe(() => {
+      this.showMenu = this.router.url.includes('/info');
+    });
+  }
 }
