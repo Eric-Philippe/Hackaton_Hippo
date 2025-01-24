25b237d471e80bcf0f531e9d0adf27c3b496775f
Bryce Fuertes <brycefuertes07@gmail.com>
2025-01-24 09:43:06 +0100

    Change service return

--- frontend/src/app/Services/chat.service.ts
+++ frontend/src/app/Services/chat.service.ts
@@ -12,77 +12,11 @@ export class ChatService {
   constructor(private http: HttpClient) {}
 
   getMessagesByZone(zone: number): Observable<Message[]> {
-    //return this.http.get<Message[]>(`${this.apiUrl}/zone/${zone}`);
-    return of([
-      {
-        username: 'Hippopotame anonyme',
-        zone: 3,
-        content: 'Salut, ça va ?',
-        time: new Date(),
-        isAdmin: false,
-      },
-      {
-        username: 'Loutre anonyme',
-        zone: 3,
-        content: 'Nickel et toi ?',
-        time: new Date(),
-        isAdmin: false,
-      },
-      {
-        username: 'Hippopotame anonyme',
-        zone: 3,
-        content: "T'as vu y a une innondation en zone 3",
-        time: new Date(),
-        isAdmin: false,
-      },
-      {
-        username: 'Loutre anonyme',
-        zone: 3,
-        content: 'Et moi je vais innonder le chat',
-        time: new Date(),
-        isAdmin: false,
-      },
-      {
-        username: 'Loutre anonyme',
-        zone: 3,
-        content: '.',
-        time: new Date(),
-        isAdmin: false,
-      },
-      {
-        username: 'Loutre anonyme',
-        zone: 3,
-        content: '.',
-        time: new Date(),
-        isAdmin: false,
-      },
-      {
-        username: 'Loutre anonyme',
-        zone: 3,
-        content: '.',
-        time: new Date(),
-        isAdmin: false,
-      },
-      {
-        username: 'Loutre anonyme',
-        zone: 3,
-        content: '.',
-        time: new Date(),
-        isAdmin: false,
-      },
-      {
-        username: 'Loutre anonyme',
-        zone: 3,
-        content: '.',
-        time: new Date(),
-        isAdmin: false,
-      },
-    ]);
+    return this.http.get<Message[]>(`${this.apiUrl}/zone/${zone}`);
   }
 
-  /**
-  sendMessage(message: Message): Observable<Message> {
-    //return this.http.post<Message>(`${this.apiUrl}/add`, message);
+  sendMessage(message: Message,zone: number): Observable<Message> {
+    return this.http.post<Message>(`${this.apiUrl}/add/${zone}`, message);
   }
-    */
+  
 }
--- frontend/src/app/Services/info.service.ts
+++ frontend/src/app/Services/info.service.ts
@@ -6,52 +6,15 @@ import { Observable, of } from 'rxjs';
   providedIn: 'root',
 })
 export class InfoService {
-  constructor() {}
+  constructor(private http: HttpClient) {}
 
-  private apiUrl = 'https://api.example.com/activities'; // R
+  private apiUrl = 'https://api.example.com/alert'; // R
 
   getDisaster(): Observable<DisasterInfo[]> {
-    // Utilisez cette ligne pour appeler l'API réelle
-    // return this.http.get<DisasterInfo[]>(this.apiUrl);
-
-    // Simulez une réponse avec des données fictives
-    return of([
-      {
-        disaster_Id: 1,
-        title: 'innondation',
-        zones: '3',
-        type: 'string',
-        date: '12/01/2180',
-        level: '3',
-      },
-      {
-        disaster_Id: 2,
-        title: 'innondation',
-        zones: '1',
-        type: 'string',
-        date: '12/01/2180',
-        level: '3',
-      },
-      {
-        disaster_Id: 3,
-        title: 'seisme',
-        zones: '3',
-        type: 'string',
-        date: '12/01/2180',
-        level: '3',
-      },
-      {
-        disaster_Id: 4,
-        title: 'seisme',
-        zones: '5',
-        type: 'string',
-        date: '12/01/2180',
-        level: '3',
-      },
-    ]);
+    return this.http.get<DisasterInfo[]>(this.apiUrl);
   }
-}
 
+}
 export interface DisasterInfo {
   disaster_Id: number;
   title: string;
